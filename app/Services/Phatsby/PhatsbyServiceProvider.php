72c8875ef28b2a9818dbf47eb2d4d8c5297a2b78
Anthony Holmes <anthony@antlur.co>
2020-11-24 00:07:06 -0600

    phatsby service provider

---
+++ app/Services/Phatsby/PhatsbyServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services\Phatsby;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+
+class PhatsbyServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $dir = getcwd();
+
+        // $this->loadViewsFrom($dir . '/src', 'site');
+
+        Route::middleware('web')
+            // ->namespace($this->namespace)
+            ->group(__DIR__ . '/routes.php');
+
+        if (File::exists(site_path('src/routes.php'))) {
+            Route::middleware('web')
+                // ->namespace($this->namespace)
+                ->group(site_path('src/routes.php'));
+        }
+    }
+}
