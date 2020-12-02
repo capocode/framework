ad8c79ee6d544ebf40d0af8450f0263a9f8f41bf
Anthony Holmes <anthony@antlur.co>
2020-12-02 00:51:37 -0600

    adjust site path

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -6,6 +6,10 @@ function site_path(?string $path = null)
 {
     $fullpath = getcwd();
 
+    if (Str::endsWith($fullpath, 'public')) {
+        $fullpath = str_replace('/public', '', $fullpath);
+    }
+
     if (Str::startsWith($path, '/')) {
         $path = ltrim($path, '/');
     }
