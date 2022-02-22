275f77f6d4158697e6515ae793863e0eb23c1eb5
Anthony Holmes <anthony@antlur.co>
2022-02-21 23:22:43 -0600

    wip

--- app/Foundation/Application.php
+++ app/Foundation/Application.php
@@ -26,10 +26,147 @@ use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Illuminate\Foundation\EnvironmentDetector;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Foundation\Application as FoundationApplication;
+use Illuminate\Foundation\PackageManifest;
+use Illuminate\Foundation\ProviderRepository;
 
 class Application extends FoundationApplication implements ApplicationContract, CachesConfiguration, CachesRoutes, HttpKernelInterface
 {
+    /**
+     * The Laravel framework version.
+     *
+     * @var string
+     */
+    const VERSION = '9.1.0';
+
+    /**
+     * The base path for the Laravel installation.
+     *
+     * @var string
+     */
+    protected $basePath;
+
+    /**
+     * Indicates if the application has been bootstrapped before.
+     *
+     * @var bool
+     */
+    protected $hasBeenBootstrapped = false;
+
+    /**
+     * Indicates if the application has "booted".
+     *
+     * @var bool
+     */
+    protected $booted = false;
+
+    /**
+     * The array of booting callbacks.
+     *
+     * @var callable[]
+     */
+    protected $bootingCallbacks = [];
+
+    /**
+     * The array of booted callbacks.
+     *
+     * @var callable[]
+     */
+    protected $bootedCallbacks = [];
+
+    /**
+     * The array of terminating callbacks.
+     *
+     * @var callable[]
+     */
+    protected $terminatingCallbacks = [];
+
+    /**
+     * All of the registered service providers.
+     *
+     * @var \Illuminate\Support\ServiceProvider[]
+     */
+    protected $serviceProviders = [];
+
+    /**
+     * The names of the loaded service providers.
+     *
+     * @var array
+     */
+    protected $loadedProviders = [];
+
+    /**
+     * The deferred services and their providers.
+     *
+     * @var array
+     */
+    protected $deferredServices = [];
+
+    /**
+     * The custom application path defined by the developer.
+     *
+     * @var string
+     */
+    protected $sitePath;
+
+    /**
+     * The custom application path defined by the developer.
+     *
+     * @var string
+     */
+    protected $appPath;
+
+    /**
+     * The custom database path defined by the developer.
+     *
+     * @var string
+     */
+    protected $databasePath;
+
+    /**
+     * The custom storage path defined by the developer.
+     *
+     * @var string
+     */
+    protected $storagePath;
+
+    /**
+     * The custom environment path defined by the developer.
+     *
+     * @var string
+     */
+    protected $environmentPath;
+
+    /**
+     * The environment file to load during bootstrapping.
+     *
+     * @var string
+     */
+    protected $environmentFile = '.env';
+
+    /**
+     * Indicates if the application is running in the console.
+     *
+     * @var bool|null
+     */
+    protected $isRunningInConsole;
+
+    /**
+     * The application namespace.
+     *
+     * @var string
+     */
+    protected $namespace;
+
+    /**
+     * The prefixes of absolute cache paths for use during normalization.
+     *
+     * @var string[]
+     */
+    protected $absoluteCachePathPrefixes = ['/', '\\'];
+
     /**
      * Create a new Illuminate application instance.
      *
@@ -282,7 +419,7 @@ class Application extends FoundationApplication implements ApplicationContract, 
         return $this;
     }
 
-   /**
+    /**
      * Get the path to the language files.
      *
      * @param  string  $path
